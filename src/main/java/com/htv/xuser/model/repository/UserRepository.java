f4094f8aa3d04df6ed474e5a1c9b2cafdb405e63
hatrongvu13 <hatrongvu13@gmail.com>
2026-03-20 15:41:39 +0700

    project running

--- src/main/java/com/htv/xuser/model/repository/UserRepository.java
+++ src/main/java/com/htv/xuser/model/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.htv.xuser.model.repository;
 
 import com.htv.xuser.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findByEmailWithRoles(String email);
+    @EntityGraph(attributePaths = {"roles", "roles.permissions"})
+    Optional<UserEntity> findByEmail(String email);
 }
